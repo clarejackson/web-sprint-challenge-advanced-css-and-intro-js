7b2f70db8425675b7a9ec46b740d5ea980bea3bd
Clare Jackson <clare-jackson@lambdastudents.com>
2020-08-30 20:54:20 -0400

    All seven JS tasks are complete. All four interview questions complete. Viewport meta tag is in the head of index.html.

--- README.md
+++ README.md
@@ -23,9 +23,20 @@ Commit your code regularly and meaningfully. This helps both you (in case you ev
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your own answers before hand.
 
 1. How would you describe acessibility on the web to someone new to programming?
-2. Talk about 3 different things you can do to ensure your website is accessible. 
+
+It is important to make all of the internet available, even to those who use adapted equipment for their computers. Some people can't use a mouse, can't see the page very well or at all, some people have a hard time reading or finding links. We can code so that all people can use the sites we create.
+
+2. Talk about 3 different things you can do to ensure your website is accessible.
+
+Accessibility is improved when alt tags are used for images, HTML structure is easy for screen readers to understand and colors are dark enough to standout from the background.
+
 3. How would you explain the concept of a variable to someone new to programming?
+
+A variable is a named container that holds data to be used later in JS functions by declaring them farther down in the code.
+
 4. What is the purpose of using functions in code?
+
+The purpose of using a function is to perform a particular task over and over inside your code faster and easier than by hard coding the same task each time. It contributes to keeping your coding DRY.
 
 You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade. 
 
--- index.js
+++ index.js
@@ -208,11 +208,14 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+artists[8].name = "Vincent Van Gogh";
+console.log(artists[8].name);
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -223,20 +226,23 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    return `The artist at index ${index} is ${array[index].name}`;
   }
-  
+  console.log(getArtistByIndex(artists, 0))
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
+function get20s(array, century){
+let century20s = [];
+  for (let i = 0; i < array.length; i++){
+    if ((array[i].years) >= century){
+      century20s.push(array[i].name);
+    }
+  } return century20s;
 }
-
+console.log(get20s(artists, "1900 - 1900"));
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
@@ -248,10 +254,12 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
-  
+function removeArtist(array, i) {
+  array.splice(i, 1);
+  return array.length;
+}
+  console.log(removeArtist(artists, 0));
+  console.log(artists);
  
 
 /**
@@ -267,11 +275,11 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/* Code here */){
-
-    /* Code here */
-
+function addArtist(array){
+    array.push({"id": 20, "name": "Clare Jackson", "years": "1982 - current day", "genre": "Web Design", "nationality": "American", "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."});
+    return array;
   }
+console.log(addArtist(artists))
 
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
@@ -281,12 +289,15 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
-
-  /* Code here */
-
+function lotsOfArt(array){
+  let mostPaintings = [];
+  for (let i = 0; i < array.length; i++){
+    if ((array[i].paintings) > 100){
+      mostPaintings.push(array[i].name);
+    }
+  } return mostPaintings;
 }
-
+console.log(lotsOfArt(artists));
 
 
 // 🎨🎨 STRETCH 🎨🎨//
